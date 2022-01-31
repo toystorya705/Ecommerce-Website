a7c2e6564a3bed8a9fe930145bdc7633530c74e5
Aryan Gupta <toystorya705@gmail.com>
2022-01-31 05:00:18 +0000

    ads added

--- productdisplay.php
+++ productdisplay.php
@@ -43,6 +43,18 @@ outputNavigation(""); // Calling Navigation Bar
         </div>
     </div>
 </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1460719726713266"
+     crossorigin="anonymous"></script>
+<!-- ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1460719726713266"
+     data-ad-slot="5408601169"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
 <script src="../JavaScript/productdisplay.js"></script>
 <?php
