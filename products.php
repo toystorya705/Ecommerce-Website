fb7a75b3a839d8f13ebe56b02514b7f3c0d74518
Aryan Gupta <toystorya705@gmail.com>
2022-01-31 07:11:04 +0000

    chnages made

--- products.php
+++ products.php
@@ -24,11 +24,25 @@ outputNavigation(""); // Calling Navigation Bar
     </div>
 
 </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1460719726713266"
+     crossorigin="anonymous"></script>
+<!-- ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1460719726713266"
+     data-ad-slot="5408601169"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 
 <div class="products" id="productid" style="margin-top:-100px;padding-bottom:30px;"><!--This create pruducts card-->
        <!-- Products Data -->
     
        </div>
+    
 <script  src="../JavaScript/products.js"></script>
 
 
